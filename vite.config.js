c98840a0a7290f1b1397f560ae50608a2e9f675a
kenichi iida <zm11616@class.kyoto-seika.ac.jp>
2026-07-16 15:38:00 +0900

    change dir

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/my-website/',    // ← あなたのリポジトリ名に変更する
   build: {
     outDir: 'docs',
     rollupOptions: {
